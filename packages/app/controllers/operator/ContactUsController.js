946bcc520af8f15422b68668a0f472bad36e9cea
Anoop Pratap Singh <anoopprsingh.social@gmail.com>
2024-11-08 21:52:44 +0530

    Update ContactUsController.js

--- packages/app/controllers/operator/ContactUsController.js
+++ packages/app/controllers/operator/ContactUsController.js
@@ -1,14 +1,11 @@
-
 const Joi = require("joi");
 const { STATUS_CODES } = require("http");
-const ContactUsService = require("../../services/ContactUsService");
 const { isValidObjectId } = require("mongoose");
+const ContactUsService = require("../../services/ContactUsService");
 
-// Define the payload schema for validating 'revert to customer' form inputs
 const revertToCustomerPayloadSchema = Joi.object().keys({
   id: Joi.string()
     .custom((value, helpers) => {
-      // Custom validation to check if the provided ID is a valid MongoDB ObjectId
       if (!isValidObjectId(value)) {
         return helpers.error("any.invalid");
       }
@@ -34,14 +31,16 @@ const revertToCustomerPayloadSchema = Joi.object().keys({
     }),
 });
 
-// Controller function to handle the revert-to-customer operation
+/**
+* Controller responsible for responding back to customer.
+* Based on there queries. Uses `id` to identify the queries
+* and on sending the reponse the customer receives an email.
+*/
 async function revertToCustomerController(req, res, next) {
-  const contactUsService = new ContactUsService(); // Initialize service to interact with form data
+  const contactUsService = new ContactUsService();
   try {
-    // Validate the request payload using the defined schema
     const { error, value } = revertToCustomerPayloadSchema.validate(req.body);
     if (error) {
-      // If validation fails, return a 422 error response with a validation error message
       return res.status(422).json({
         error: STATUS_CODES[422],
         message: error.message,
@@ -50,25 +49,22 @@ async function revertToCustomerController(req, res, next) {
     }
 
     const { id, reply } = value;
-    const { userId } = req.userData; // Extract user ID from request metadata
-    // Update the form entry with the provided reply, ID, and user ID
+    const { userId } = req.userData;
     const revertForm = await contactUsService.updateForm(id, reply, userId);
     if (revertForm?.alreadyReplied) {
-      // If a response was already sent for this form, return a 409 conflict error
       return res.status(409).json({
         statusCode: 409,
         error: STATUS_CODES[409],
         message: "Already sent the response for this query!",
       });
     }
-    // If successful, send a 200 status response with the updated form data
+
     return res.status(200).json({
       statusCode: 200,
       message: "Form updated successfully",
       data: revertForm,
     });
   } catch (error) {
-    // Pass any server errors to the next middleware
     next(error);
   }
 }
